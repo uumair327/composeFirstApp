105ad31619997ec6f9eda2da6ff72d1375fa48fb
uumair327 <mohdumair.a@somaiya.edu>
2023-12-20 17:38:11 +0530

    composableDemo

--- app/src/main/java/com/example/composefirstapp/MainActivity.kt
+++ app/src/main/java/com/example/composefirstapp/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeFirstAppTheme {
-        Greeting("Android")
+        Greeting("Composable")
     }
 }
